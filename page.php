be6267778cbfbe23dc24a0a757496cfdbaf30606
AdfieldMatt <matt@adfield.co.uk>
2017-03-29 10:20:07 +0100

    170329 Posts & Service Area Styles
    Posts have now been added.
    The service area has been restyled to use flex box.

--- page.php
+++ page.php
@@ -89,9 +89,10 @@ get_header(); ?>
   <div class="row">
     <div class="col-md-6 no-padding">
       <div id="news_area">
-        <?php while (have_posts()) : the_post(); ?>
+        <?php $the_query = new WP_Query( 'posts_per_page=2' ); ?>
+        <?php while ($the_query -> have_posts()) : $the_query -> the_post(); ?>
           <div class="post">
-            <h3><?php the_title(); ?></h3>
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
             <h4>Posted by <?php the_author(); ?> <span class="readmore"><a href="<?php the_permalink(); ?>" class="readmore">Read More</a></span></h4>
           </div>
         <?php endwhile;?>
@@ -117,84 +118,43 @@ get_header(); ?>
   </div>
 </div>
 
-  <div class="container-fluid hidden-xs">
-    <div class="row">
-      <div class="col-md-4 col-sm-6 col-xs-12 no-padding">
-        <div class="grid">
-          <figure class="effect-zoe">
-            <h3>Motor Repairs</h3>
-            <img src="<?php bloginfo('template_directory'); ?>/includes/img/conveyor-oil.jpg" alt="Food Industry Conveyor Repairs"/>
-            <figcaption>
-              <h2>Motor <span>Repairs</span></h2>
-              <p class="icon-links">
-                <a href="#"><span class="icon-eye"></span></a>
-              </p>
-              <p class="description">The electrical department specialises in electrically driven industrial rotating equipment...</p>
-            </figcaption>     
-          </figure>
+<div class="container-fluid">
+  <div class="row">
+    <div class="service_area_grid">
+      <div class="service_area_item" style="background:url('<?php bloginfo('template_directory'); ?>/includes/img/conveyor-oil.jpg'); background-size:cover;">
+        <div class="overlay">
+          <h3>Repairs</h3>
+          <a href="#" class="btn-primary">Discover</a>
         </div>
       </div>
-      <div class="col-md-4 col-sm-6 col-xs-12 no-padding">
-        <div class="grid">
-          <figure class="effect-zoe">
-            <h3>Maintenance</h3>
-            <img src="<?php bloginfo('template_directory'); ?>/includes/img/industrial-fan.jpg" alt="Industrial Fans"/>
-            <figcaption>
-              <h2>Maintenance</h2>
-              <p class="icon-links">
-                <a href="#"><span class="icon-eye"></span></a>
-              </p>
-              <p class="description">With maintenance crews being reduced and under more pressure to cover a wide range of plant and equipment...</p>
-            </figcaption>     
-          </figure>
+      <div class="service_area_item" style="background:url('<?php bloginfo('template_directory'); ?>/includes/img/industrial-fan.jpg'); background-size:cover;">
+        <div class="overlay">
+          <h3>Maintenance</h3>
+          <a href="#" class="btn-primary">Discover</a>
         </div>
       </div>
-      <div class="col-md-4 col-sm-6 col-xs-12 no-padding">
-        <div class="grid">
-          <figure class="effect-zoe">
-            <h3>Manufacturing</h3>
-            <img src="<?php bloginfo('template_directory'); ?>/includes/img/factory-motor.jpg" alt="Factory Motors"/>
-            <figcaption>
-              <h2>Manufacturing</h2>
-              <p class="icon-links">
-                <a href="#"><span class="icon-eye"></span></a>
-              </p>
-              <p class="description">Where motor or generators have special requirements due to performance criteria, size or environment...</p>
-            </figcaption>     
-          </figure>
+      <div class="service_area_item" style="background:url('<?php bloginfo('template_directory'); ?>/includes/img/factory-motor.jpg'); background-size:cover;">
+        <div class="overlay">
+          <h3>Manufacturing</h3>
+          <a href="#" class="btn-primary">Discover</a>
         </div>
       </div>
-      <div class="col-md-6 col-sm-6 col-xs-12 no-padding">
-        <div class="grid">
-          <figure class="effect-zoe">
-            <h3>Product Sales</h3>
-            <img src="<?php bloginfo('template_directory'); ?>/includes/img/product-motors.jpg" alt="Product Motors"/>
-            <figcaption>
-              <h2>Product <span>Sales</span></h2>
-              <p class="icon-links">
-                <a href="#"><span class="icon-eye"></span></a>
-              </p>
-              <p class="description">Our new sales department of manufacturer trained, experienced internal sales engineers is dedicated to the sales of new machines...</p>
-            </figcaption>     
-          </figure>
+    </div>
+    <div class="service_area_grid">
+      <div class="service_area_item"style="background:url('<?php bloginfo('template_directory'); ?>/includes/img/product-motors.jpg'); background-size:cover;">
+        <div class="overlay">
+          <h3>Product Sales</h3>
+          <a href="#" class="btn-primary">Discover</a>
         </div>
       </div>
-      <div class="col-md-6 col-sm-6 col-xs-12 no-padding">
-        <div class="grid">
-          <figure class="effect-zoe">
-            <h3>Energy Saving</h3>
-            <img src="<?php bloginfo('template_directory'); ?>/includes/img/wind-turbine.jpg" alt="Wind Turbines Energy Saving"/>
-            <figcaption>
-              <h2>Energy <span>Saving</span></h2>
-              <p class="icon-links">
-                <a href="#"><span class="icon-eye"></span></a>
-              </p>
-              <p class="description">It is a little known fact that an electric motor uses up to 100 times its purchase cost in electricity during it’s working life...</p>
-            </figcaption>     
-          </figure>
+      <div class="service_area_item"style="background:url('<?php bloginfo('template_directory'); ?>/includes/img/wind-turbine.jpg'); background-size:cover;">
+        <div class="overlay">
+          <h3>Energy Saving</h3>
+          <a href="#" class="btn-primary">Discover</a>
         </div>
       </div>
     </div>
   </div>
+</div>
 
 <?php get_footer(); ?>
